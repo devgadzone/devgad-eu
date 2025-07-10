c04180f641bb1d72366630028db66759920ee303
Dariusz Gał <dgal@devgadzone.pl>
2025-07-10 20:03:38 +0200

    feat: Add FrontMatter model and GlobalUsings for cleaner code

---
+++ generator/BlogGenerator/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using BlogGenerator.Models;
+//global using BlogGenerator.Services;
+//global using BlogGenerator.Utils;
+global using YamlDotNet.Serialization;
+global using Markdig;
---
+++ generator/BlogGenerator/Models/FrontMatter.cs
@@ -0,0 +1,43 @@
+﻿namespace BlogGenerator.Models;
+
+public class FrontMatter
+{
+    [YamlMember(Alias = "title")]
+    public string? Title { get; set; }
+
+    [YamlMember(Alias = "date")]
+    public DateTime? Date { get; set; }
+
+    [YamlMember(Alias = "slug")]
+    public string? Slug { get; set; }
+
+    [YamlMember(Alias = "draft")]
+    public bool? Draft { get; set; }
+
+    [YamlMember(Alias = "author")]
+    public string? Author { get; set; }
+
+    [YamlMember(Alias = "excerpt")]
+    public string? Excerpt { get; set; }
+
+    [YamlMember(Alias = "tags")]
+    public List<string>? Tags { get; set; }
+
+    [YamlMember(Alias = "categories")]
+    public List<string>? Categories { get; set; }
+
+    [YamlMember(Alias = "description")]
+    public string? Description { get; set; }
+
+    [YamlMember(Alias = "image")]
+    public string? Image { get; set; }
+
+    [YamlMember(Alias = "canonical")]
+    public string? Canonical { get; set; }
+
+    [YamlMember(Alias = "layout")]
+    public string? Layout { get; set; }
+
+    [YamlMember(Alias = "updated")]
+    public DateTime? Updated { get; set; }
+}
